0a1ae2eb90bae7702c21d9bec4c998272c8557e0
oscarliusy <1069176850@qq.com>
2020-06-18 14:28:35 +0800

    cors port

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ const config = {
     cors_config:{
       origin: (ctx) => {
         let origin = ctx.request.headers.origin || ''
-        const allowedOrigins = ['http://localhost:3000','file://']
+        const allowedOrigins = ['http://localhost:3000','http://localhost:80','file://']
         if (allowedOrigins.includes(origin) || origin.includes('localhost')) {
           return origin
         } else {
