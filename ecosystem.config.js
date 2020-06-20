b751e7dd2d665e4d934e962270a6a4cf3e050715
oscarliusy <1069176850@qq.com>
2020-06-20 14:21:12 +0800

    run.sh

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: 'erpServer',
+    name: 'erpServerAuto',
     script: 'start.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
