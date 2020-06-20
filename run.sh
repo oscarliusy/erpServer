a72b276591a618009828d193ac8bc1d21012dd62
oscarliusy <1069176850@qq.com>
2020-06-20 16:21:49 +0800

    test

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -ilex
 
 export SERVER_NAME=erpServerAuto
+export npm=/usr/local/src/.nvm/versions/node/v12.18.0/bin/npm
+export pm2=/usr/local/src/.nvm/versions/node/v12.18.0/bin/pm2
 
-npm install
 
-if ! pm2 restart $SERVER_NAME; then
-  pm2 start ecosystem.config.js --env production
+$npm install
+
+if ! $pm2 restart $SERVER_NAME; then
+  $pm2 start ecosystem.config.js --env production
 fi
