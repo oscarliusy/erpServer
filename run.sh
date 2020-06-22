8e1ffa27f1b04a4ec9e81b0e79a60eb9af1ef754
oscarliusy <1069176850@qq.com>
2020-06-22 10:31:55 +0800

    jenkins test

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -ilex
 
 export SERVER_NAME=erpServerAuto
-export npm=/usr/local/src/.nvm/versions/node/v12.18.0/bin/npm
-export pm2=/usr/local/src/.nvm/versions/node/v12.18.0/bin/pm2
 
+pm2 stop $SERVER_NAME
 
-$npm install
+npm install
 
-if ! $pm2 restart $SERVER_NAME; then
-  $pm2 start ecosystem.config.js --env production
-fi
+npm run prd
+
